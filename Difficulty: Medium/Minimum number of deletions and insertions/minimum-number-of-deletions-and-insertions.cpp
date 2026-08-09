5d80d2199ec9af0689ef0e0711e15e291aa14be7
yash <yashparmar012007@gmail.com>
2026-08-09 15:13:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.cpp
@@ -0,0 +1,45 @@
+class Solution {
+
+  public:
+  
+  
+    int helper(string s1,string s2,int n, int m,int idx1,int idx2,vector<vector<int>>&dp){
+        
+        
+        
+        if(idx1==n || idx2==m){
+            
+            return 0;
+            
+        }
+        
+        if(dp[idx1][idx2]!=-1){
+            return dp[idx1][idx2];
+        }
+        
+        if(s1[idx1]==s2[idx2]){
+            return 1+helper(s1,s2,n,m,idx1+1,idx2+1,dp);
+        }
+        
+        int move1=helper(s1,s2,n,m,idx1+1,idx2,dp);
+        int move2=helper(s1,s2,n,m,idx1,idx2+1,dp);
+        
+        return dp[idx1][idx2]=max(move1,move2);
+        
+        
+        
+        
+        
+    }
+  
+  
+  
+    int minOperations(string &s1, string &s2) {
+        // Your code goes here 
+        int n=s1.size();
+        int m=s2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+     int lcs= helper(s1,s2,n,m,0,0,dp);
+     return (n-lcs)+(m-lcs);
+    }
+};
